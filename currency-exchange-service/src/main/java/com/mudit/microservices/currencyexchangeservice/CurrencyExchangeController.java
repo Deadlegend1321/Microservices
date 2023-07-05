84e717312901bcdf119b5ceb16302c28c09b595f
Deadlegend1321 <mudit1318@outlook.com>
2023-07-05 19:39:24 +0530

    Using Zipkin Docker Container for Micrometer Tracing and Creating Docker Images of the microservices

--- currency-exchange-service/src/main/java/com/mudit/microservices/currencyexchangeservice/CurrencyExchangeController.java
+++ currency-exchange-service/src/main/java/com/mudit/microservices/currencyexchangeservice/CurrencyExchangeController.java
@@ -5,11 +5,15 @@ import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.math.BigDecimal;
 
 @RestController
 public class CurrencyExchangeController {
+
+    private Logger logger = LoggerFactory.getLogger(CurrencyExchangeController.class);
 
     @Autowired
     private CurrencyExchangeRepository repository;
@@ -19,6 +23,9 @@ public class CurrencyExchangeController {
     @GetMapping("/currency-exchange/from/{from}/to/{to}")
     public CurrencyExchange retrieveExchangeValue(@PathVariable String from,
                                                   @PathVariable String to){
+
+        logger.info("retrieveExchangeValue called with {} to {}", from, to);
+
         CurrencyExchange currencyExchange = repository.findByFromAndTo(from, to);
         if(currencyExchange == null){
             throw new RuntimeException("Unable to Find data for " + from + " to " + to);
